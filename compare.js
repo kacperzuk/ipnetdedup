d10d2744b107ddedcb8bcf4b8aed67757cca293b
Kacper Żuk <kacper.b.zuk@gmail.com>
2015-10-18 01:41:19 +0200

    Merge adjacent networks

--- compare.js
+++ compare.js
@@ -3,6 +3,26 @@
 const ip = require("ip");
 const ipcheck = require("ipcheck");
 
+function comp(arr1, arr2) {
+  for(let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i])
+      return false;
+  }
+  return true;
+}
+
+function addressMinus(addr, v) {
+  for(let i = addr.length - 1; i >= 0; i--) {
+    addr[i] -= v;
+    v = 1;
+    if(addr[i] >= 0) {
+      return addr;
+    }
+    else addr[i] += 256;
+  }
+  return addr;
+}
+
 module.exports = function(a, b) {
   let nA, nB;
   try {
@@ -29,6 +49,26 @@ module.exports = function(a, b) {
        return a;
      else
        return b;
+   }
+
+   if(nA.length == nB.length) {
+     let candidate = null;
+     if(comp(nA.lastAddress.address, addressMinus(nB.firstAddress.address, 3)))  {
+       // nA first, nB next
+       candidate = { a: nA.networkAddress, m: nA.subnetMaskLength - 1 };
+     } else if(comp(nB.lastAddress.address, addressMinus(nA.firstAddress.address, 3))) {
+       // nB first, nA next
+       candidate = { a: nB.networkAddress, m: nB.subnetMaskLength - 1 };
+     }
+
+     if(candidate) {
+       let candidate_network = ip.mask(candidate.a, ip.fromPrefixLen(candidate.m));
+       if(candidate.a == candidate_network) {
+         candidate = `${candidate.a}/${candidate.m}`;
+         console.warn(a, b, "grouped:", candidate);
+         return candidate;
+       }
+     }
    }
 
    return false;
